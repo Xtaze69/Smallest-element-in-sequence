22ba319ef1642559eb565869dec7ff29a9c4c146
Xtaze <Xtaze@DESKTOP-2DTRJ02>
2024-04-10 00:49:43 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Наименьший_элемент_в_последовательности
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+            int number;
+            int min = int.MaxValue;
+
+            Console.WriteLine("Введите длину последовательности: ");
+            length = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("Введите число: ");
+                number = int.Parse(Console.ReadLine());
+                min = min > number ? number : min;
+            }
+
+            Console.WriteLine($"Минимальное из чисел = {min}");
+            Console.ReadKey();
+        }
+    }
+}
